8077208e249713d44cce61586c551de4ec41f2ee
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-04-29 23:09:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n=s.size();
+        unordered_<char>st;
+        st.insert('a');
+        st.insert('');
+        st.insert('');
+        st.insert('');
+        st.insert('');
+        
+    }
+};
