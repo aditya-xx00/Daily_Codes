e86f6690e8100b1cbd21b5673b52e751dcefae2a
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-06-15 15:23:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.84%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -2,31 +2,19 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         
-        unordered_map<char,bool>mp;
-        for(char & x:order){
-            mp[x]=true;
+        unordered_map<char,int>mp;
+        for(int i=0;i<order.size();i++){
+            mp[order[i]]=i;
         }
-        unordered_map<char,int>mp1;
-        for(char & x:s){
-            mp1[x]++;
-        }
-        string ans;
-        int j=0;
-        for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])!=mp.end()){
-                while(j<order.size() && mp1[order[j]]>0){
-                  ans.push_back(order[j]);
-                    mp1[order[j]]--;
-                }
-               // mp.erase(s[i]);
-                j++;
-            }
-        }
-        for(char &x: s){
-            if(mp.find(x)==mp.end()){
-                ans.push_back(x);
-            }
-        }
-        return ans;
+        
+        auto mycomparator=[&mp] (char & ch1,char &ch2){
+            if(mp[ch1]<mp[ch2])
+             return true;
+            else
+             return false; 
+        } ;
+
+        sort(s.begin(),s.end(),mycomparator);
+        return s;
     }
 };
