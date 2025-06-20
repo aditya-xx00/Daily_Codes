e5427346deb06d24241a45007c28ed7e953752ab
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-06-20 15:35:47 +0530

    Time: 4 ms (37.06%), Space: 16.4 MB (18.31%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void parenthesis(int l,int zero,int one,vector<string>&ans,string temp,int n){
+        if(zero==n and one ==n){
+            ans.push_back(temp);
+            return;
+        }
+        if(one>n)
+          return ;
+        if(zero>one){
+            return ;
+        }
+        parenthesis(l-1,zero+1,one,ans,temp+')',n);
+        parenthesis(l-1,zero,one+1,ans,temp+'(',n);
+    }
+    vector<string> generateParenthesis(int n) {
+         int l=2*n;
+         int zero=0;
+         int one=0;
+         vector<string>ans;
+         string temp;
+          parenthesis(l,zero,one,ans,temp,n);
+          return ans;
+    }
+};
