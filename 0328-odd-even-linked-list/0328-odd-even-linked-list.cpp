df47ca36c431a0fe3b64a39bf2de929ffeb17711
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-07-19 13:15:30 +0530

    Time: 0 ms (100%), Space: 15.5 MB (62.99%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,36 +11,17 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL) return nullptr;
-        ListNode* odd=head;
-        ListNode* even=head->next;
-        ListNode* dummy=new ListNode(-1);
-        ListNode* tail=dummy;
-        while(odd && odd->next!=nullptr){
-            ListNode* newNode=new ListNode(odd->val);
-            tail->next=newNode;
-            tail=tail->next;
-            odd=odd->next->next;
+        if(!head || !head->next)
+         return head;
+        ListNode* odd=head, *even=head->next;
+        ListNode *evenhead=even;
+        while(even && even->next){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
         }
-        if(odd){
-          ListNode* newNode=new ListNode(odd->val);
-            tail->next=newNode;
-            tail=tail->next;
-        }
-         
-         while(even && even->next!=nullptr){
-            ListNode* newNode=new ListNode(even->val);
-            tail->next=newNode;
-            tail=tail->next;
-            even=even->next->next;
-        }
-        if(even) {
-             ListNode* newNode=new ListNode(even->val);
-            tail->next=newNode;
-            tail=tail->next;
-        }
-        ListNode* result=dummy->next;
-        delete dummy;
-        return result;
+        odd->next=evenhead;
+        return head;
     }
 };
