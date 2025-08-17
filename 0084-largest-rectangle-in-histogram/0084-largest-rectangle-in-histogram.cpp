eb7dbcc4939f3063877fe760d1fe64856c195a09
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-08-17 12:16:32 +0530

    Time: 34 ms (24.24%), Space: 91.9 MB (9.71%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
+    vector<int> nse(vector<int> nums){
+        int n=nums.size();
+        vector<int>res(n);
+        stack<int>st;
+        
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && nums[i]<=nums[st.top()]){
+               st.pop();
+            }
+            if(st.empty()) res[i]=n;
+            else
+            res[i]=st.top();
+           st.push(i);
+        }
+        return res;
+    }
+
+     vector<int> pse(vector<int> nums){
+        int n=nums.size();
+        vector<int>res(n);
+        stack<int>st;
+        for(int i=0; i<n; i++){
+         while(!st.empty() && nums[i]<=nums[st.top()]){
+               st.pop();
+            }
+            if(st.empty()) res[i]=-1;
+            else
+            res[i]=st.top();
+           st.push(i);
+        }
+        return res;
+    }
+
     int largestRectangleArea(vector<int>& heights) {
-       stack<int>st;
-       int n=heights.size();
-       vector<int>NSR(n);
-       vector<int>NSL(n);
-
-       for(int i=0;i<n;i++){
-        while(!st.empty() && heights[i]<heights[st.top()]){
-            NSR[st.top()]=i;
-            st.pop();
+        
+        vector<int>v1=nse(heights), v2=pse(heights);
+        int maxi=0;
+        for(int i=0;i<heights.size();i++){
+           int ans=heights[i]*(v1[i]-v2[i]-1);
+           maxi=max(maxi,ans);
         }
-        st.push(i);
-       } 
-       while(!st.empty()){
-        NSR[st.top()]=n;
-        st.pop();
-       }
-
-        for(int i=n-1;i>=0;i--){
-        while(!st.empty() && heights[i]<heights[st.top()]){
-            NSL[st.top()]=i;
-            st.pop();
-        }
-        st.push(i);
-       }
-
-        while(!st.empty()){
-        NSL[st.top()]=-1;
-        st.pop();
-       }
-
-       int maxi=0;
-       for(int i=0;i<n;i++){
-        int area=heights[i]*(NSR[i]-NSL[i]-1);
-         maxi=max(maxi,area);
-       }
-       return maxi;
+        return maxi;
     }
 };
