299be9ffa131c8b23dbd005dc9bf0de1d5264b18
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-27 17:33:09 +0530

    Time: 3 ms (71.11%), Space: 13 MB (88.48%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void parenthesis(int l,int zero,int one,vector<string>&ans,string temp,int n){
-        if(zero==one && l==0){
+    void Comb(vector<string> & ans, int l,int r, string &temp,int n){
+        if(temp.size()==2*n) {
             ans.push_back(temp);
             return;
+        } 
+        if(r<n){
+            temp.push_back('(');
+             Comb(ans,l,r+1,temp,n);
+             temp.pop_back();
         }
-        if(one>n)
-          return ;
-        if(zero>one){
-            return ;
-        }
-        parenthesis(l-1,zero+1,one,ans,temp+')',n);
-        parenthesis(l-1,zero,one+1,ans,temp+'(',n);
+        if(l<r){
+            temp.push_back(')');
+             Comb(ans,l+1,r,temp,n);
+             temp.pop_back();
+        }     
     }
     vector<string> generateParenthesis(int n) {
-         int l=2*n;
-         int zero=0;
-         int one=0;
-         vector<string>ans;
-         string temp;
-          parenthesis(l,zero,one,ans,temp,n);
-          return ans;
+        vector<string> ans;
+        int len=2*n-1;
+        int l=0,r=0;
+        string temp="";
+        Comb(ans,l,r,temp,n);
+        return ans;
     }
 };
