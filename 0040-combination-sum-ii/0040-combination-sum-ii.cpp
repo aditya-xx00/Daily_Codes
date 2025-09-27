6ed16d2887a72a1210b889122e56e2ff6fad74e8
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-27 23:34:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.79%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-   void Solve(int idx,int sum ,vector<int>&temp,vector<vector<int>>& result,vector<int>&candidates , int target){
-       if(sum==target){
-      //  sort(temp.begin(),temp.end());
-        result.push_back(temp);
-          return;
-       }
-       if( sum>target || idx >=candidates.size()){
-         return ;
-       }
-      
-       temp.push_back(candidates[idx]);
-       Solve(idx+1,sum+candidates[idx],temp,result,candidates,target);
-       temp.pop_back();
-       
-        while(idx+1 < candidates.size() && candidates[idx]==candidates[idx+1])
-          idx=idx+1  ;
-       Solve(idx+1,sum,temp,result,candidates,target);
+    void Combination(vector<vector<int>>&ans, vector<int>&temp, int sum,vector<int>& candidates, int target,int i){
+          if(target==sum){
+             ans.push_back(temp);
+             return ;
+          }
+          if(sum>target || i>=candidates.size()) return ;
+          
+          temp.push_back(candidates[i]);
+          sum+=candidates[i];
+          Combination(ans,temp,sum,candidates,target,i+1);
+          sum-=candidates[i];
+          temp.pop_back();
 
-   }
+          while(i<candidates.size()-1 && candidates[i]==candidates[i+1]){
+               i=i+1;
+          }
+          Combination(ans,temp,sum,candidates,target,i+1);
+    }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
+        vector<vector<int>>ans;
+        sort(candidates.begin(),candidates.end());
         vector<int>temp;
-         sort(candidates.begin(),candidates.end());
-        int sum=0;
-        int idx=0;
-        Solve(idx,sum,temp,result,candidates,target);
-        sort(result.begin(),result.end());
-         return  result;    
+        
+        Combination(ans,temp,0,candidates,target,0);
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
