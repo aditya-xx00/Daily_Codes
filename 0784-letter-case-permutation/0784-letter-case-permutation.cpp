f1f68343be8f106778e95f78970dd62431ee3676
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-27 22:15:34 +0530

    Time: 4 ms (38.18%), Space: 15.4 MB (40.83%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void Permute(string s,string temp,vector<string>&ans){
-        if(s.size()==0){
+    void Letter(vector<string>& ans, string & temp, string s,int i){
+        if(i==s.size()){
             ans.push_back(temp);
             return;
         }
-        string s1=temp;
-        string s2=temp;
-        if(isdigit(s[0]))
-        {
-          s1+=s[0];
-          s.erase(0,1);
-          Permute(s,s1,ans);
-        }else{
-           s1+=tolower(s[0]);
-           s2+=toupper(s[0]);
-           s.erase(0,1);
-           Permute(s,s1,ans);
-        Permute(s,s2,ans);
-        }
-        
-    }
+        if(isalpha(s[i])){
+             temp.push_back(tolower(s[i]));
+             Letter(ans,temp,s,i+1);
+             temp.pop_back();
 
+              temp.push_back(toupper(s[i]));
+             Letter(ans,temp,s,i+1);
+             temp.pop_back();
+        }else{
+           temp.push_back(s[i]);
+            Letter(ans,temp,s,i+1);
+            temp.pop_back();
+        }
+       
+    }
     vector<string> letterCasePermutation(string s) {
-        
         vector<string>ans;
         string temp="";
-        Permute(s,temp,ans);
-         return ans;
+        Letter(ans,temp,s,0);
+        return ans;
     }
 };
