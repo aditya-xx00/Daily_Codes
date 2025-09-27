b9433e819d99ef4cb43878f2eed3a912906bbbcd
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-27 23:17:14 +0530

    Time: 6 ms (32.79%), Space: 18.3 MB (23.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-   void Solve(int idx,int sum ,vector<int>temp,vector<vector<int>>& result,vector<int>&candidates , int target){
-       if(sum==target){
-        result.push_back(temp);
-          return;
-       }
-       if( sum>target){
-         return ;
-       }
-       if(idx>=candidates.size())
-        return ;
-         
-       Solve(idx+1,sum,temp,result,candidates,target);
-       temp.push_back(candidates[idx]);
-       sum+=candidates[idx];
-       Solve(idx,sum,temp,result,candidates,target);
-
-   }
+    void Combination(set<vector<int>>&ans, vector<int>&temp, int sum,vector<int>& candidates, int target,int i){
+          if(target==sum){
+             ans.insert(temp);
+             return ;
+          }
+          if(sum>target || i>=candidates.size()) return ;
+          
+          Combination(ans,temp,sum,candidates,target,i+1);
+          temp.push_back(candidates[i]);
+          sum+=candidates[i];
+          Combination(ans,temp,sum,candidates,target,i);
+          Combination(ans,temp,sum,candidates,target,i+1);
+          temp.pop_back();
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
+        set<vector<int>>ans;
         vector<int>temp;
-        int sum=0;
-        int idx=0;
-        Solve(idx,sum,temp,result,candidates,target);
-        return result;
-
+        
+        Combination(ans,temp,0,candidates,target,0);
+        return vector<vector<int>> (ans.begin(),ans.end());
     }
 };
