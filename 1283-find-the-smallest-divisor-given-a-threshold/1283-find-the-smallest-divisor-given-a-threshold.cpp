743ac8005c38bd2dd595a165e531dcc6b6fe8090
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-28 20:14:23 +0530

    Time: 7 ms (69.03%), Space: 26.1 MB (78.89%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    bool IsAns(vector<int>& nums ,int threshold,int mid){
+    
+    long long divisor(vector<int>& nums, int mid){
+        int n=nums.size();
         int ans=0;
-         for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             if(nums[i]%mid==0)
-              ans+=nums[i]/mid;
-            else
-             ans+=(nums[i]/mid)+1;  
-         }
-         if(threshold>=ans)
-          return true;
-         else
-          return false; 
+               ans+=(nums[i]/mid);
+             else
+                ans+=(nums[i]/mid+1); 
+        }
+        return ans;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
+        int n=nums.size();
+        int start=1;
         int ans=-1;
-        int maxi=*max_element(nums.begin(),nums.end());
+        int maxi=INT_MIN;
+        
+        for(int i=0;i<n;i++){
+           maxi=max(maxi,nums[i]);
+        }
 
-        int start=1,end=maxi;
+        int end=maxi;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(IsAns(nums,threshold,mid)==true)
-            {
+            if(divisor(nums,mid)<=threshold){
                 ans=mid;
                 end=mid-1;
             }else{
                 start=mid+1;
-            } 
+            }
         }
         return ans;
     }
