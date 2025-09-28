095d03e19118bb7bcbc6facf5d488f0d126cb78e
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-28 11:23:17 +0530

    Time: 11 ms (49.57%), Space: 12.3 MB (41.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,20 +6,19 @@ public:
         int i=0,j=0;
         int maxi=0;
         while(j<n){
-           mp[s[j]]++;
+            mp[s[j]]++;
 
-           if(mp.size()==j-i+1){
-             maxi=max(maxi,j-i+1);
-           }
-           else if(j-i+1>mp.size()){
-              while(j-i+1>mp.size()){
-                  mp[s[i]]--;
-                  if(mp[s[i]]==0)
-                   mp.erase(s[i]);
-                  i++; 
-              }
-           }
-           j++;
+            if(mp.size()==j-i+1)
+            maxi=max(maxi,j-i+1);
+
+            else if(mp.size()!=j-i+1){
+                while(mp.size()<j-i+1){
+                   mp[s[i]]--;
+                   if(mp[s[i]]==0) mp.erase(s[i]);
+                   i++;
+                }
+            }
+            j++;
         }
         return maxi;
     }
