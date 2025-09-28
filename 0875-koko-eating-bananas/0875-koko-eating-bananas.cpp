1a5a280bb650f499517a56b639b4efa8cbc66a38
Aditya Kushwaha <153971336+aditya-xx00@users.noreply.github.com>
2025-09-28 20:01:19 +0530

    Time: 10 ms (44.32%), Space: 23 MB (42.54%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    int eat(vector<int>piles, int mid){
-        int ans=0;
+     
+     long long time(vector<int>& piles, int mid){
+        long long t=0;
         for(int i=0;i<piles.size();i++){
-            int k=piles[i]/mid;
-            if(piles[i]%mid ==0)
-              ans+=k;
-            else
-              ans+=(k+1);  
+            if(piles[i]%mid!=0){
+                t+=piles[i]/mid;
+                t+=1;
+            }else
+              t+=piles[i]/mid;
+        }
+        return t;
+     }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int maxi=INT_MIN;
+
+        for(int i=0;i<n;i++){
+           maxi=max(maxi,piles[i]);
+        }
+         int ans=maxi;
+        int start=1, end=maxi;
+        while(start<=end){
+           int mid=start+(end-start)/2;
+            if(time(piles,mid)<=h){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                 start=mid+1;
+            }
         }
         return ans;
-    }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int start=1;
-        int maxi=INT_MIN;
-        for(int i=0;i<piles.size();i++){
-            maxi=max(maxi,piles[i]);
-        }
-        int end=maxi;
-        int res=INT_MAX;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(eat(piles,mid)<=h){
-               res=mid;
-               end=mid-1;
-            }
-            else
-              start=mid+1;
-        }
-        return res;
     }
 };
